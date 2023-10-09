4043662f4e976ffd7b4eda94bb3aac7c5d76c3c0
Thomas M. DuBuisson <tdubuisson@paxos.com>
2023-10-09 15:22:06 -0700

    Logs pubkey on successful log in and allow concatenating u2f_keys file

--- util.c
+++ util.c
@@ -230,13 +230,8 @@ static int parse_native_format(const cfg_t *cfg, const char *username,
     if (s_user && strcmp(username, s_user) == 0) {
       debug_dbg(cfg, "Matched user: %s", s_user);
 
-      // only keep last line for this user
-      for (i = 0; i < *n_devs; i++) {
-        reset_device(&devices[i]);
-      }
-      *n_devs = 0;
-
-      i = 0;
+      // keep all lines each user, concatenating later auth patterns
+      i = n_devs;
       while ((s_credential = strtok_r(NULL, ":", &saveptr))) {
         if ((*n_devs)++ > cfg->max_devs - 1) {
           *n_devs = cfg->max_devs;
@@ -1254,6 +1249,7 @@ int do_authentication(const cfg_t *cfg, const device_t *devices,
           }
           r = fido_assert_verify(assert, 0, pk.type, pk.ptr);
           if (r == FIDO_OK) {
+            debug_dbg(cfg, "Successful FIDO authentication with publicKey %s (idx %u)", devices[i].publicKey, i);
             retval = 1;
             goto out;
           }
